b2a760575ca770c4a5111b40da73188e3f89cf2e
Haley Visconti <hvisconti1343@outlook.com>
2025-04-09 11:28:28 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sorei9240/quiz-game
+
+go 1.24.1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"flag"
+)
+
+func main() {
+	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	flag.Parse()
+	_ = csvFilename
+}
